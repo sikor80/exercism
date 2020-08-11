ce814beaa0fa546b42e4e1723ca00a0bdb67ce93
sikor <3697792+sikor80@users.noreply.github.com>
2020-08-11 15:59:57 +1000

    Update

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,29 +1,27 @@
 // Package scrabble is a solution to exercism.io exercise titled Scrabble Score.
 package scrabble
 
-import "strings"
+import "unicode"
 
 // Score computes the Scrabble score for a provided word.
 func Score(str string) int {
 	var result int
 	for _, r := range str {
-		switch strings.ToUpper(string(r)) {
-		case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
 			result = result + 1
-		case "D", "G":
+		case 'D', 'G':
 			result = result + 2
-		case "B", "C", "M", "P":
+		case 'B', 'C', 'M', 'P':
 			result = result + 3
-		case "F", "H", "V", "W", "Y":
+		case 'F', 'H', 'V', 'W', 'Y':
 			result = result + 4
-		case "K":
+		case 'K':
 			result = result + 5
-		case "J", "X":
+		case 'J', 'X':
 			result = result + 8
-		case "Q", "Z":
+		case 'Q', 'Z':
 			result = result + 10
-		default:
-			result = result + 0
 		}
 	}
 
